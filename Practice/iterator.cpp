f914d157bcb84af10771dc3d04b92dbd2d28476b
Shima1371 <zebracoco1371@icloud.com>
2021-08-18 06:28:30 +0900

    output_all適用後のdestinationが出力されるよう修正

--- Practice/iterator.cpp
+++ Practice/iterator.cpp
@@ -6,15 +6,17 @@ auto output_all = [](auto first, auto last, auto output_iter)
 	{
 		*output_iter = *iter;
 	}
-	system("pause");
 };
 
 int main()
 {
-	//o—Í‚³‚ê‚È‚¢
 	std::vector<int> source = { 1,2,3,4,5 };
 	std::vector<int> destination(5);
 
 	output_all(std::begin(source), std::end(source), std::begin(destination));
+	for (int i = 0; i != 5; i++)
+	{
+		std::cout << destination.at(i) << " "s;
+	}
 	system("pause");
 }
